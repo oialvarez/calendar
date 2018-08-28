503cf53b9655d72d7256d96d4d900a0c63095ef4
oialvarez <oialvarez@gmail.com>
2018-08-28 00:40:20 -0300

    setting default date

--- calendar.js
+++ calendar.js
@@ -1,13 +1,22 @@
 const DAYS = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-
+const MONTHS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const render = () => {
-  const startDate = document.getElementById('startDate') || new Date().toISOString().split('T')[0];
+  const startDateInput = document.getElementById('startDate');
+  const startDateValue = startDateInput.value || new Date().toISOString().split('T')[0];
+  console.log('ssss', startDateInput.value)
+  console.log('ssss', startDateValue)
+  if (!startDateInput.value) {
+    startDateInput.value = startDateValue;
+  }
+
   const numberOfDays = document.getElementById('numberOfDays') || 17;
   const country = document.getElementById('country') || 'US';
   const calendar = document.getElementById('theCalendar');
 
   const table = document.createElement('div');
   const tableHeader = document.createElement('div');
+  const date = document.createElement('div');
+  table.appendChild(date);
   table.appendChild(tableHeader);
 
   for (const DAY in DAYS) {
